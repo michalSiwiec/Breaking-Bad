26551bc2cf600de8b23fcfd8504f5c998861918b
michalSiwiec <siwiec.michal724@gmail.com>
2020-07-21 17:10:08 +0200

    ass

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -34,59 +34,11 @@ import {BrowserRouter,Link,NavLink,Route} from 'react-router-dom'
 
 class App extends React.Component {
 
-  constructor(props){
-    super(props);
-    // this.state = {
-    //   selectedMenuOption: 1,
-    //   menuOptionClasses: ["selected",undefined,undefined,undefined]
-    // }
-  }
-
-  selectMenuOption = (e) => {
-    
-    let menuOptionBuffor,
-    menuOptionClassesBuffor = [undefined,undefined,undefined,undefined];
-
-    switch(e.target.id){
-
-      case "menuOption1":
-        menuOptionBuffor = 1;
-        menuOptionClassesBuffor[0] = "selected";
-        break;
-
-      case "menuOption2":
-        menuOptionBuffor = 2;
-        menuOptionClassesBuffor[1] = "selected";
-        break;
-
-      case "menuOption3":
-        menuOptionBuffor = 3;
-        menuOptionClassesBuffor[2] = "selected";
-        break;
-
-      case "menuOption4":
-        menuOptionBuffor = 4;
-        menuOptionClassesBuffor[3] = "selected";
-        break;
-
-      default:
-        break;
-
-    }
-
-    this.setState({
-      selectedMenuOption: menuOptionBuffor,
-      menuOptionClasses: menuOptionClassesBuffor
-    })
-
-  }
-
-
   render(){
 
     return (
       <BrowserRouter>
-        <Menu selectOption={this.selectMenuOption} menuOptionClasses={this.state.menuOptionClasses}/>
+        <Menu/>
         {/* <MainContent selectedOption={this.state.selectedMenuOption}/> */}
         <Footer/>
       </BrowserRouter>
